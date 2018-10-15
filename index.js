d2e90f3448965e98b585720289454178a47fcd44
Rohit Gohri <rohit.gohri@smartprix.com>
2018-10-15 18:44:52 +0530

    process listeners to Oak

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+import {System} from 'sm-utils';
 import Oak from './Oak';
+
+process.on('uncaughtException', (error) => {
+	Oak.error('Uncaught Exception', error);
+	System.exit(1);
+});
+
+process.on('unhandledRejection', (error) => {
+	Oak.error('Unhandled Rejection', error);
+});
+
+process.on('exit', (code) => {
+	Oak.info({code}, 'Process exited with code', code);
+	System.exit(code);
+});
+
+process.on('SIGINT', () => {
+	Oak.info('Received SIGINT.');
+	System.exit('Received SIGINT');
+});
+
+process.on('SIGTERM', () => {
+	Oak.info('Received SIGTERM.');
+	System.exit('Received SIGTERM');
+});
 
 export default Oak;
